db37ac2104db5706ec654949f8a99026f4d977bd
markaschell <mark.a.schell@gmail.com>
2016-09-01 22:28:16 -0400

    Another upcoming Task

---
+++ code/SoftwareThresher/SoftwareThresher/Tasks/NotFoundInCSharpProject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftwareThresher.Observations;
+
+namespace SoftwareThresher.Tasks
+{
+    public class NotFoundInCSharpProject : Task
+    {
+        public string ReportTitleForErrors
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public List<Observation> Execute(List<Observation> observations)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
