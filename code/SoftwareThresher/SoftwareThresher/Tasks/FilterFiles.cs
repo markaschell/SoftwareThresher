cd78993edd024decd0de9c9e84d444a9438a683d
markaschell <mark.a.schell@gmail.com>
2016-09-01 22:29:56 -0400

    Add Filter task

---
+++ code/SoftwareThresher/SoftwareThresher/Tasks/FilterFiles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using SoftwareThresher.Observations;
+
+namespace SoftwareThresher.Tasks
+{
+    public class FilterFiles : Task
+    {
+        public string ReportTitleForErrors
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public List<Observation> Execute(List<Observation> observations)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
