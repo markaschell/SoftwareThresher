1f3113924363a9ef9ef42b9e659ffa7b8e96bc1f
markaschell <mark.a.schell@gmail.com>
2016-09-05 21:45:45 -0400

    Update Filter task to only filter on location for now
    Updated name to reflect the future usage

---
+++ code/SoftwareThresher/SoftwareThresher/Tasks/Filter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using SoftwareThresher.Observations;
+
+namespace SoftwareThresher.Tasks {
+   public class Filter : Task, NoDetailsInReport {
+
+      // TODO - make nullable when the next parameter is added
+      public string LocationRegExPattern { get; set; }
+
+      public string ReportTitle { get { return "Items Filtered"; } }
+
+      public List<Observation> Execute(List<Observation> observations) {
+         var regex = new Regex(LocationRegExPattern, RegexOptions.RightToLeft | RegexOptions.Singleline);
+
+         return observations.FindAll(o => !regex.IsMatch(o.Location));
+      }
+   }
+}
---
+++ code/SoftwareThresher/SoftwareThresherTests/Tasks/FilterTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoftwareThresher.Observations;
+using SoftwareThresher.Tasks;
+
+namespace SoftwareThresherTests.Tasks {
+   [TestClass]
+   public class FilterTests {
+
+      Filter filter;
+
+      [TestInitialize]
+      public void Setup() {
+         filter = new Filter();
+      }
+
+      [TestMethod]
+      public void Execute_FiltersLocationRegExPattern() {
+         filter.LocationRegExPattern = "a";
+
+         var result = filter.Execute(new List<Observation> { new FileObservation(@"C:\akdkk\this is it") });
+
+         Assert.AreEqual(0, result.Count);
+      }
+
+      [TestMethod]
+      public void Execute_DoesNotFilterLocationRegExPattern() {
+         filter.LocationRegExPattern = "i";
+
+         var results = filter.Execute(new List<Observation> { new FileObservation(@"C:\akdkk\this is it") });
+
+         Assert.AreEqual(1, results.Count);
+         Assert.IsFalse(results.First().Failed);
+      }
+   }
+}
