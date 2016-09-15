07c4bb4281d739e4b9fe4da136256a2ced222739
markaschell <mark.a.schell@gmail.com>
2016-09-14 21:37:38 -0400

    Add SystemFileReader

---
+++ code/SoftwareThresher/SoftwareThresher/Utilities/SystemFileReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace SoftwareThresher.Utilities {
+
+   public interface ISystemFileReader {
+      void Open(string filename);
+      string ReadLine();
+      void Close();
+   }
+
+   class SystemFileReader : ISystemFileReader {
+      StreamReader streamReader;
+
+      public void Open(string filename) {
+         if (streamReader != null) {
+            throw new InvalidOperationException("Opening a document when the last one is still open.");
+         }
+
+         streamReader = new StreamReader(filename);
+      }
+
+      public string ReadLine() {
+         return streamReader.ReadLine();
+      }
+
+      public void Close() {
+         streamReader.Close();
+         streamReader = null;
+      }
+   }
+}
