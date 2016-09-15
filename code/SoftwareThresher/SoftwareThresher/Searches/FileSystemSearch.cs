42c62eaa54e6460cf625902a3222994c25e4f475
markaschell <mark.a.schell@gmail.com>
2016-09-14 21:45:22 -0400

    Setup to test FileSystemSearch

--- code/SoftwareThresher/SoftwareThresher/Searches/FileSystemSearch.cs
+++ code/SoftwareThresher/SoftwareThresher/Searches/FileSystemSearch.cs
@@ -2,9 +2,19 @@
 using System;
 using System.IO;
 using System.Linq;
+using SoftwareThresher.Utilities;
 
 namespace SoftwareThresher.Searches {
    public class FileSystemSearch : Search {
+
+      ISystemFileReader systemFileReader;
+
+      public FileSystemSearch() : this(new SystemFileReader()) { }
+
+      public FileSystemSearch(ISystemFileReader systemFileReader) {
+         this.systemFileReader = systemFileReader;
+      }
+
       public List<string> GetFiles(string directory, string searchPattern) {
          return Directory.EnumerateFiles(directory, searchPattern, SearchOption.AllDirectories).ToList();
       }
---
+++ code/SoftwareThresher/SoftwareThresherTests/Searches/FileSystemSearchTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using SoftwareThresher.Searches;
+using SoftwareThresher.Utilities;
+
+namespace SoftwareThresherTests.Searches {
+   [TestClass]
+   public class FileSystemSearchTests {
+      ISystemFileReader reader;
+
+      FileSystemSearch search;
+
+      [TestInitialize]
+      public void Setup() {
+         reader = Substitute.For<ISystemFileReader>();
+
+         search = new FileSystemSearch(reader);
+      }
+      [TestMethod]
+      public void TestMethod1() {
+      }
+   }
+}
