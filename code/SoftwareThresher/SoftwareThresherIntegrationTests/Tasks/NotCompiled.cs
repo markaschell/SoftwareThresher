65d46e2a359f143b3e5d1af0af0ea51ccd451377
markaschell <mark.a.schell@gmail.com>
2016-09-19 23:43:19 -0400

    Not Compiled Integration Test

---
+++ code/SoftwareThresher/SoftwareThresherIntegrationTests/Tasks/NotCompiled.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SoftwareThresherIntegrationTests.Tasks {
+   [TestClass]
+   public class NotCompiledTests : TaskTestBase {
+      [TestMethod]
+      public void NotCompiled() {
+         RunTest("NotCompiled.xml");
+
+         Assert.AreEqual(-1, LastReportItem.ChangeInObservations);
+         Assert.AreEqual(1, LastReportItem.FailedObservations.Count);
+         Assert.AreEqual("ClassNotIncludedInTheProject.cs", LastReportItem.FailedObservations.First().Name);
+      }
+   }
+}
