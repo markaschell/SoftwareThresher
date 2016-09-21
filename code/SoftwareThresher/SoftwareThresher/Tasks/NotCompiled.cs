465cf474029752fe8b3c54389304de5d437d6cb9
markaschell <mark.a.schell@gmail.com>
2016-09-20 21:12:59 -0400

    Use a lookup in NotCompiled for performance

--- code/SoftwareThresher/SoftwareThresher/Tasks/NotCompiled.cs
+++ code/SoftwareThresher/SoftwareThresher/Tasks/NotCompiled.cs
@@ -28,18 +28,18 @@ namespace SoftwareThresher.Tasks {
       public List<Observation> Execute(List<Observation> observations) {
          observations.ForEach(o => o.Failed = true);
 
-         // TODO - 1 Performance - Use a hash map for obervations
+         var observationsLookup = observations.ToLookup(o => o.Location);
          // TODO - Last Performance - multi-threaded
-         search.GetFiles(Directory, CompileConfigurationFileSearchPattern).ForEach(file => MarkObservationsPassedForFile(observations, file));
+         search.GetFiles(Directory, CompileConfigurationFileSearchPattern).ForEach(file => MarkObservationsPassedForFile(observationsLookup, file));
 
          return observations;
       }
 
-      void MarkObservationsPassedForFile(IEnumerable<Observation> observations, string file) {
+      void MarkObservationsPassedForFile(ILookup<string, Observation> observations, string file) {
          var fileDirectory = new FileObservation(file).Location;
          // TODO - 2 Performance - use a hashmap or something similar
          // TODO - this performance does not support the use of relative paths in the found items....Should I need to handle this situation or can I get performance other ways?  Or should that be flagged because that is a bad practice.  Should this be reported via some other means?
-         var observationsWithSameDirectory = observations.Where(o => o.Location.StartsWith(fileDirectory));
+         var observationsWithSameDirectory = observations.Where(o => o.Key.StartsWith(fileDirectory)).SelectMany(g => g);
 
          search.GetReferencesInFile(file, TextSearchPattern).ForEach(r => MarkObservationsPassedForReference(observationsWithSameDirectory, fileDirectory, r));
       }
