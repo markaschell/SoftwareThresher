18ee91ca04503695ba5022355d1e2e651c3e7dc3
markaschell <mark.a.schell@gmail.com>
2016-10-30 22:52:12 -0400

    Add OpenGrokObservation

---
+++ code/SoftwareThresher/SoftwareThresher/Observations/OpenGrokObservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace SoftwareThresher.Observations {
+   public class OpenGrokObservation : Observation {
+      readonly string directory;
+
+      public OpenGrokObservation(string directory, string filename)
+      {
+         this.directory = directory;
+         this.Name = filename;
+      }
+
+      public override string Location => directory.Replace(@"\\\", string.Empty);
+
+      public override string Name { get; }
+
+      public override string ToString() {
+         return Location + '/' + Name;
+      }
+   }
+}
