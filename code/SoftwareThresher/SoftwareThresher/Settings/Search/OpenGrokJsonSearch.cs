c8f3eb82ee7c079b4ee522fe2e878ad3f964ce2e
markaschell <mark.a.schell@gmail.com>
2016-11-01 22:19:46 -0400

    Make parameter creation generic
    Clean up line results and ignore items with no value

--- code/SoftwareThresher/SoftwareThresher/Settings/Search/OpenGrokJsonSearch.cs
+++ code/SoftwareThresher/SoftwareThresher/Settings/Search/OpenGrokJsonSearch.cs
@@ -6,23 +6,29 @@ using System.Runtime.Serialization.Json;
 using System.Text;
 using SoftwareThresher.Observations;
 
+// TODO - Add to the usage - Current searching does not support file extentions very well so we have to filter out until then
+// On Upgrade of Opengrok (current version ) check the following:
+//   - if formatting is still in line value
+//   - if searching by path with an extension works.  ex - ".cs"
 namespace SoftwareThresher.Settings.Search {
 
    public class OpenGrokJsonSearch : Search {
 
-      const string TextSearchParameterLabel = "freetext=";
-      //const string DefinitionSearchParameterLabel = "def=";
-      //const string SymbolSearchParameterLabel = "symbol=";
-      const string PathSearchParameterLabel = "path=";
-      //const string HistorySearchParameterLabel = "hist=";
-      const string MaxResultsParameterLabel = "maxresults=";
+      const string TextSearchParameterLabel = "freetext";
+      //const string DefinitionSearchParameterLabel = "def";
+      //const string SymbolSearchParameterLabel = "symbol";
+      const string PathSearchParameterLabel = "path";
+      //const string HistorySearchParameterLabel = "hist";
+      const string MaxResultsParameterLabel = "maxresults";
       const string ParameterJoin = "&";
-      const int MaxResults = 1000000; // In theroy could go as high as java max Integer
+      const string MaxResults = "1000000"; // In theroy could go as high as java max Integer
 
       public string BaseLocation { private get; set; }
 
       public List<Observation> GetObservations(string location, string searchPattern) {
-         return GetResults($"{PathSearchParameterLabel}\"{GetPathSearchPattern(location, searchPattern)}\"").ConvertAll(r => (Observation)new OpenGrokObservation(r.directory, r.filename));
+         var parameters = new List<OpenGrokParameter> { new OpenGrokParameter(PathSearchParameterLabel, GetPathSearchPattern(location, searchPattern)) };
+
+         return GetResults(parameters).ConvertAll(r => (Observation)new OpenGrokObservation(r.directory, r.filename));
       }
 
       string GetPathSearchPattern(string location, string searchPattern) {
@@ -34,13 +40,18 @@ namespace SoftwareThresher.Settings.Search {
       }
 
       public List<string> GetReferenceLine(Observation observation, string searchPattern) {
-         return GetResults($"{PathSearchParameterLabel}\"{observation.SystemSpecificString}\"{ParameterJoin}{TextSearchParameterLabel}\"{searchPattern}\"")
-                .Where(r => !string.IsNullOrEmpty(r.lineno)).ToList()
-                .ConvertAll(r => r.line);
+         var parameters = new List<OpenGrokParameter>
+         {
+            new OpenGrokParameter(PathSearchParameterLabel, observation.SystemSpecificString),
+            new OpenGrokParameter(TextSearchParameterLabel, searchPattern)
+         };
+
+         return GetResults(parameters).Where(r => !string.IsNullOrEmpty(r.lineno)).Select(r => r.line).ToList();
       }
 
-      List<OpenGrokJsonSearchResult> GetResults(string parameters) {
-         var request = WebRequest.Create($"{BaseLocation}/json?{parameters}{ParameterJoin}{MaxResultsParameterLabel}{MaxResults}");
+      List<OpenGrokJsonSearchResult> GetResults(ICollection<OpenGrokParameter> parameters) {
+         var parameterString = BuildParameterString(parameters);
+         var request = WebRequest.Create($"{BaseLocation}/json?{parameterString}");
 
          using (var response = request.GetResponse()) {
             var serializer = new DataContractJsonSerializer(typeof(OpenGrokJsonSearchResponse));
@@ -52,6 +63,29 @@ namespace SoftwareThresher.Settings.Search {
 
             return searchResponse.results;
          }
+      }
+
+      // TODO - test any of this? - move to another class
+      static string BuildParameterString(ICollection<OpenGrokParameter> parameters) {
+         parameters.Add(new OpenGrokParameter(MaxResultsParameterLabel, MaxResults));
+
+         return parameters.Aggregate(string.Empty, (current, parameter) => current + ParamaterString(parameter, string.IsNullOrEmpty(current)));
+      }
+
+      static string ParamaterString(OpenGrokParameter parameter, bool firstParameter) {
+         var parameterJoin = firstParameter ? string.Empty : ParameterJoin;
+
+         return $"{parameterJoin}{parameter.Label}=\"{parameter.Value}\"";
+      }
+
+      public class OpenGrokParameter {
+         public OpenGrokParameter(string label, string value) {
+            Label = label;
+            Value = value;
+         }
+
+         public string Label { get; }
+         public string Value { get; }
       }
 
       // As defined here: https://github.com/OpenGrok/OpenGrok/wiki/OpenGrok-web-services
@@ -69,7 +103,10 @@ namespace SoftwareThresher.Settings.Search {
          string _line;
          public string line {
             get { return _line; }
-            set { _line = Encoding.UTF8.GetString(Convert.FromBase64String(value)); }
+            set {
+               var readableString = Encoding.UTF8.GetString(Convert.FromBase64String(value));
+               _line = readableString.Replace("</b>", string.Empty);
+            }
          }
          //public string path { get; set; }
       }
