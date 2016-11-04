c2caaaf3bfcc278c5e7ec4c01b6f944028ff231f
markaschell <mark.a.schell@gmail.com>
2016-11-03 20:21:13 -0400

    Add Tests around OpenGrokParameter

---
+++ code/SoftwareThresher/SoftwareThresherTests/Settings/Search/OpenGrokParameterTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoftwareThresher.Settings.Search;
+
+namespace SoftwareThresherTests.Settings.Search {
+   [TestClass]
+   public class OpenGrokParameterTests {
+      [TestMethod]
+      public void ToString_NoFormatting()
+      {
+         const string label = "fin";
+         const string value = "it";
+
+         var parameter = new OpenGrokParameter(label, value);
+
+         Assert.AreEqual("fin=it", parameter.ToString());
+      }
+
+      [TestMethod]
+      public void ToString_ValueContainsAQuoteVariablee_EscapesQuote() {
+         const string value = "it\"s";
+
+         var parameter = new OpenGrokParameter("one", value);
+
+         Assert.IsTrue(parameter.ToString().EndsWith("=it\\\"s"));
+      }
+      [TestMethod]
+      public void ToString_ValueContainsSpaces_PutsQuotesAroundTheValue() {
+         const string value = "it is";
+
+         var parameter = new OpenGrokParameter("one", value);
+
+         Assert.IsTrue(parameter.ToString().EndsWith("=\"it is\""));
+      }
+   }
+}
