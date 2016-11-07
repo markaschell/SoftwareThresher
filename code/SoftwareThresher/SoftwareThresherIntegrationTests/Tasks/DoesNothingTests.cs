e007d1c8096fb050082f52ef264798317a9f4a91
markaschell <mark.a.schell@gmail.com>
2016-11-06 22:21:19 -0500

    Add Integration Test for no settings or tasks

---
+++ code/SoftwareThresher/SoftwareThresherIntegrationTests/Tasks/DoesNothingTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SoftwareThresherIntegrationTests.Tasks {
+   [TestClass]
+   public class DoesNothingTests : TaskTestBase {
+      [TestMethod]
+      public void DoesNothing_NoResult() {
+         RunTest("DoesNothing.xml");
+
+         Assert.IsNull(LastReportItem);
+      }
+   }
+}
